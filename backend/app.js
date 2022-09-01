ec54593705871f99e5e48326db43746ad7c006b7
Михаил Кукреш <kukreshmihal@yandex.ru>
2022-09-01 21:29:07 +0300

    fix: error

--- backend/app.js
+++ backend/app.js
@@ -40,8 +40,8 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
-console.log(process.env.NODE_ENV); // production
-console.log(process.env.JWT_SECRET); // production
+/// console.log(process.env.NODE_ENV); // production
+/// console.log(process.env.JWT_SECRET); // production
 
 app.use(cors());
 
